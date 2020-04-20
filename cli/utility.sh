48a4d3e8a6be7bdd4df50a0752d3b433bfd5e909
Bryan Yee <bryan.yee@grandrounds.com>
2020-04-20 09:21:19 -0700

    Add notes for gem & npm pacakge versions, and killing a process

--- cli/utility.sh
+++ cli/utility.sh
@@ -1,4 +1,5 @@
 ### Searching
+
 # Case-insensitive, recursive search for text in files.
 rg -i <string>
 ag -i <string>
@@ -7,3 +8,22 @@ grep -i -IR <string> .
 # Case-insensitive, recursive search for file names.
 ag -g <string>
 find . -name <pattern>
+
+
+### Rails
+
+# Find ruby gem version.
+bundle show <gem>
+
+
+### JS
+
+yarn list --pattern <pattern> # Find npm package vesrion.
+yarn why <package>            # Find why a version(s) of a package is included in yarn.lock.
+yarn info <package>           # Fetch info about a package, including latest stable version.
+
+### Misc.
+
+# Kill a process
+ps | grep -i <search>
+kill -9 <pid>
