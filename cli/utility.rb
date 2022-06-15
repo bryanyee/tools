9515c1d362027e77f0800a0d40b187219a133bf6
Bryan Yee <yee.bryan.c@gmail.com>
2022-06-15 16:31:24 -0700

    Add steps to perform or rollback a rails migration with a version number

--- cli/utility.rb
+++ cli/utility.rb
@@ -3,3 +3,10 @@ file_name = 'test_file.json'
 file = File.open(File.join(Dir.pwd, "/tmp/#{file_name}"), "w+") do |f|
  f << 'test string'
 end
+
+
+# Run migration with a specific version
+db:migrate VERSION=20080906120000
+
+# Rollback migration with a specific version
+rails db:migrate:down VERSION=20080906120000
