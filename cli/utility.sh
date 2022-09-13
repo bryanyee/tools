e1d0ad3ed0952bfbc994056237b77cfd2437f599
Bryan Yee <yee.bryan.c@gmail.com>
2022-09-12 17:15:30 -0700

    Add xcode-select notes

--- cli/utility.sh
+++ cli/utility.sh
@@ -25,6 +25,7 @@ yarn list --pattern <pattern> # Find npm package vesrion.
 yarn why <package>            # Find why a version(s) of a package is included in yarn.lock.
 yarn info <package>           # Fetch info about a package, including latest stable version.
 
+
 ### Misc.
 
 # File permissions
@@ -39,3 +40,14 @@ kill -9 <pid>         # Kill the process.
 
 # scp
 scp remote_host:path/to/remote_file path/to/local_directory
+
+# xcode-select - manage the active developer directory
+xcode-select -p              # print the path of the active developer directory
+xcode-select --switch <path>  # set the path for the active developer directory
+
+# Switching paths resolved the error:
+# Failed to locate 'git', requesting installation of command line developer tools.
+
+# Likely paths for xcode-select:
+# /Applications/Xcode.app/Contents/Developer
+# /Library/Developer/CommandLineTools
